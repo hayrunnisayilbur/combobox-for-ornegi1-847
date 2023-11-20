525fd6253e9a905bcac58613950b6ab71a619d0c
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-11-20 10:04:41 +0300

    Add files via upload

---
+++ combobox for örneği1-847/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace combobox_for_örneği1_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbCiftSayilar = new System.Windows.Forms.ComboBox();
+            this.btnAktar = new System.Windows.Forms.Button();
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cmbCiftSayilar
+            // 
+            this.cmbCiftSayilar.FormattingEnabled = true;
+            this.cmbCiftSayilar.Location = new System.Drawing.Point(23, 311);
+            this.cmbCiftSayilar.Name = "cmbCiftSayilar";
+            this.cmbCiftSayilar.Size = new System.Drawing.Size(131, 21);
+            this.cmbCiftSayilar.TabIndex = 0;
+            // 
+            // btnAktar
+            // 
+            this.btnAktar.Location = new System.Drawing.Point(23, 247);
+            this.btnAktar.Name = "btnAktar";
+            this.btnAktar.Size = new System.Drawing.Size(131, 43);
+            this.btnAktar.TabIndex = 1;
+            this.btnAktar.Text = "çift sayıları aktar";
+            this.btnAktar.UseVisualStyleBackColor = true;
+            this.btnAktar.Click += new System.EventHandler(this.btnSayilar_Click);
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.Location = new System.Drawing.Point(23, 27);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(131, 199);
+            this.lbSayilar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbSayilar);
+            this.Controls.Add(this.btnAktar);
+            this.Controls.Add(this.cmbCiftSayilar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCiftSayilar;
+        private System.Windows.Forms.Button btnAktar;
+        private System.Windows.Forms.ListBox lbSayilar;
+    }
+}
+
---
+++ combobox for örneği1-847/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combobox_for_örneği1_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); // formu calıştırır.
+            // 20 sayıyı burada oluşturabilir. metot bilgisi gerekiyor.
+        }
+
+        // butonunu tıklayınca calıştırır.
+
+
+        private void btnSayilar_Click(object sender, EventArgs e)
+        {
+            cmbCiftSayilar.Items.Clear();
+            // rastgele oluşturulmus olan sayılardan çift olanlarını aktar
+            for (int i = 0; i < 20; i++)
+            {
+                int sayi = Convert.ToInt32(lbSayilar.Items[i]);
+                if (sayi % 2 == 0)
+                {
+                    cmbCiftSayilar.Items.Add(sayi);
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // rastgele 20 sayı olustur
+            Random rnd = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                lbSayilar.Items.Add(rnd.Next(1, 101));
+            }
+        }
+    }
+}
